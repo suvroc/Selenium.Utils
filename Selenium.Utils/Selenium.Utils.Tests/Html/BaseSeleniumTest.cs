f84389d610f87a53536e27fa8546301e2c2917b6
suvroc <suvroc@o2.pl>
2017-04-18 20:47:46 +0200

    Table element

---
+++ Selenium.Utils/Selenium.Utils.Tests/Html/BaseSeleniumTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium.Utils.Tests.Html
+{
+    public class BaseSeleniumTest
+    {
+        protected IWebDriver _driver;
+        private string _page;
+
+        public BaseSeleniumTest(string page)
+        {
+            _page = page;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            CreateDriver();
+        }
+
+        private void CreateDriver()
+        {
+            _driver = new ChromeDriver();
+            
+            _driver.Navigate().GoToUrl($"{AppDomain.CurrentDomain.BaseDirectory}\\TestFiles\\{_page}");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
---
+++ Selenium.Utils/Selenium.Utils.Tests/Html/TableTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenium.Utils.Html;
+using System.Threading;
+
+namespace Selenium.Utils.Tests.Html
+{
+    [TestFixture]
+    public class TableTest : BaseSeleniumTest
+    {
+        public TableTest() : base("table.html")
+        {
+
+        }
+
+        [Test]
+        public void should_get_table_data()
+        {
+            var table = _driver.FindTable(By.Id("tableId"));
+
+            Assert.AreEqual("2.1", table.GetData(2, 1).Text);
+        }
+
+        [Test]
+        public void should_get_table_columns()
+        {
+            var table = _driver.FindTable(By.Id("tableId"));
+
+            Assert.That(table.Columns, Is.EquivalentTo(new string[] { "Col 1", "Col 2", "Col 3" }));
+        }
+    }
+}
