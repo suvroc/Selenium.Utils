d89c05fb4806aea6b6c25459c6c6626fbabd8cad
suvroc <suvroc@o2.pl>
2017-04-23 20:16:27 +0200

    List element

---
+++ Selenium.Utils.Tests/Html/ListTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenium.Utils.Html;
+using System.Linq;
+using System.Threading;
+
+namespace Selenium.Utils.Tests.Html
+{
+    [TestFixture]
+    public class ListTest : BaseSeleniumTest
+    {
+        public ListTest() : base("list.html")
+        {
+
+        }
+
+        [Test]
+        public void should_get_all_ul_list_items()
+        {
+            var table = new ListElement(_driver, By.Id("unorderedList"));
+
+            Assert.AreEqual(4, table.Items.Count());
+        }
+
+        [Test]
+        public void should_get_all_ol_list_items()
+        {
+            var table = new ListElement(_driver, By.Id("orderedList"));
+
+            Assert.AreEqual(3, table.Items.Count());
+        }
+    }
+}
---
+++ Selenium.Utils/Html/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace Selenium.Utils.Html
+{
+    public class ListElement : BaseElement
+    {
+        public ListElement(IWebDriver driver, By selector) : base(driver, selector)
+        {
+        }
+
+        public ListElement(IWebDriver driver, IWebElement element) : base(driver, element)
+        {
+        }
+
+        public IEnumerable<IWebElement> Items
+        {
+            get
+            {
+                return Element.FindElements(By.TagName("li"));
+            }
+        }
+    }
+}
