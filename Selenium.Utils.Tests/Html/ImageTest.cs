b7a52afada344c662ec1a4ac50e412b441d96fb4
suvroc <suvroc@o2.pl>
2017-04-23 20:53:13 +0200

    Image element

---
+++ Selenium.Utils.Tests/Html/ImageTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenium.Utils.Html;
+using System.Threading;
+using System.Linq;
+
+namespace Selenium.Utils.Tests.Html
+{
+    [TestFixture]
+    public class ImageTest : BaseSeleniumTest
+    {
+        public ImageTest() : base("image.html")
+        {
+
+        }
+
+        [Test]
+        public void should_check_image()
+        {
+            var select = new Image(_driver, By.Id("image"));
+
+            Assert.IsTrue(select.Source.EndsWith("somefile.png"));
+        }
+    }
+}
---
+++ Selenium.Utils/Html/Image.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace Selenium.Utils.Html
+{
+    public class Image : BaseElement
+    {
+        public Image(IWebDriver driver, By selector) : base(driver, selector)
+        {
+        }
+
+        public Image(IWebDriver driver, IWebElement element) : base(driver, element)
+        {
+        }
+
+        public string Source
+        {
+            get
+            {
+                return Element.GetAttribute("src");
+            }
+        }
+
+        public string Alt
+        {
+            get
+            {
+                return Element.GetAttribute("alt");
+            }
+        }
+    }
+}
