bf43430a4cf187417ed0ab3e99c5e12e1932bae9
suvroc <suvroc@o2.pl>
2017-04-26 19:58:26 +0200

    More extensions

--- Selenium.Utils/Extensions/ElementExtension.cs
+++ Selenium.Utils/Extensions/ElementExtension.cs
@@ -11,6 +11,24 @@ namespace Selenium.Utils.Extensions
 {
     public static class ElementExtensions
     {
+        public static void UploadFile(this IWebElement element, string filepath)
+        {
+            if (element.GetAttribute("type") != "file")
+            {
+                throw new ArgumentException("You can't upload file for the input with different type than 'file'");
+            }
+            element.SendKeys(filepath);
+        }
         // execute js
+
+        public static object GetJsProperty(this IWebElement element, IWebDriver driver, string jsCode)
+        {
+            var jsExecutor = driver as IJavaScriptExecutor;
+            if (jsExecutor != null)
+            {
+                return jsExecutor.ExecuteScript(string.Concat("return arguments[0].", jsCode), element);
+            }
+            return null;
+        }
     }
 }
