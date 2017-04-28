b433111748efeed1880b1505d40c0647c42a95ac
Adrian Sroka <adrian.sroka@britishcouncil.pl>
2017-04-28 10:44:46 +0200

    Test file download

---
+++ Selenium.Utils.Tests/Helpers/HelperTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Selenium.Utils.Extensions;
+using Selenium.Utils.Helpers;
+using Selenium.Utils.Tests.Html;
+
+namespace Selenium.Utils.Tests.Helpers
+{
+    [TestFixture]
+    public class HelperTest
+    {
+        [Test]
+        [TestCase("http://www.google.com", true)]
+        [TestCase("http://w2w2w.g2o2o2g22l2e.c", false)]
+        public void should_test_file_download(string url, bool result)
+        {
+            var res = GeneralHelper.TestFileDownload(url);
+
+            Assert.AreEqual(result, res);
+        }
+
+
+    }
+}
---
+++ Selenium.Utils/Helpers/GeneralHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Utils.Helpers
+{
+    public class GeneralHelper
+    {
+        public static bool TestFileDownload(string url)
+        {
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse webResponse;
+            try
+            {
+                webResponse = webRequest.GetResponse();
+            }
+            catch //If exception thrown then couldn't get response from address
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
